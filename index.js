40ef55a053382d607419e701382d312a974590a7
Crystal <muthonigachanja2000@gmail.com>
2023-06-22 14:23:40 -0700

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+
+function customerName(){
+    return customerName
+}
+var customerName= "bob";
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+    return customerName;
+} 
+
+function setBestCustomer(){
+    bestCustomer = 'not bob'
+    return customerName;
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+    return customerName;
+}
+
+const leastFavoriteCustomer = 'jose';
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'bob'; 
+    return leastFavoriteCustome;
+}
